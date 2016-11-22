7bf06102395cead947986f1f04a484a8ab16338f
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-11-22 17:59:39 -0500

    almost complete, still some problems with the tax rounding

--- Tw/src/main/Main.java
+++ Tw/src/main/Main.java
@@ -16,27 +16,29 @@ public class Main {
 	public static String currentLine;
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		// create a cart that will checkout and print the receipt.
-		theCart = new Cart();
-		
-		//open the first input file and create a Scanner that will go through each item in the list.
-		inputOne = new File("/Users/Sushi24/my-projects/JAVA/Tw/resources/input2.txt");
-
-		scan = new Scanner(inputOne);
-		
-		// instantiate the currentItem to the first item in the list.
-		currentLine = scan.nextLine();
-		theCart.addItem(new Item(currentLine));
-		
-		//loop through the items in the input and add them to the cart.
-		while(scan.hasNext()) {
+		for(int i = 1 ; i < 4 ; i++) {
+			// create a cart that will checkout and print the receipt.
+			theCart = new Cart();
+			
+			//open the first input file and create a Scanner that will go through each item in the list.
+			inputOne = new File("/Users/Sushi24/my-projects/JAVA/Tw/resources/input" + i + ".txt");
+	
+			scan = new Scanner(inputOne);
+			
+			// instantiate the currentItem to the first item in the list.
 			currentLine = scan.nextLine();
 			theCart.addItem(new Item(currentLine));
+			
+			//loop through the items in the input and add them to the cart.
+			while(scan.hasNext()) {
+				currentLine = scan.nextLine();
+				theCart.addItem(new Item(currentLine));
+			}
+			
+			scan.close();
+			
+			theCart.checkOut(); //print the receipt.
+			System.out.println("================================================\n");
 		}
-		
-		scan.close();
-		
-		theCart.checkOut(); //print the receipt.
 	}
 }
